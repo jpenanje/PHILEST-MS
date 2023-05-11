4e7361e744860a13778312f418915c2a294a28b6
Penanje Philip <jpenanje@gmail.com>
2023-05-11 12:23:59 +0100

    updated forms

--- src/main/java/com/sms/repositories/Repository.java
+++ src/main/java/com/sms/repositories/Repository.java
@@ -7,7 +7,7 @@ public class Repository {
     public static JsonNode getMenuItems() {
         String menuString = '['+
         "{\"title\": \"Dashboard\",\"selectedImgPath\": \"/images/dashboard_icon_light.png\", \"unselectedImgPath\": \"/images/dashboard_icon_dark.png\", \"controller\":\"com.sms.controllers.DashboardController\",\"view\":\"/sections/Dashboard.fxml\"},"+
-        "{\"title\": \"Students\",\"selectedImgPath\": \"/images/dashboard_icon_light.png\", \"unselectedImgPath\": \"/images/dashboard_icon_dark.png\", \"controller\":\"com.sms.controllers.StudentsController\",\"view\":\"/sections/TableSection.fxml\", \"headers\":[\"Header1\",\"Header2\"]},"+
+        "{\"title\": \"Pupils\",\"selectedImgPath\": \"/images/dashboard_icon_light.png\", \"unselectedImgPath\": \"/images/dashboard_icon_dark.png\", \"controller\":\"com.sms.controllers.StudentsController\",\"view\":\"/sections/TableSection.fxml\", \"headers\":[\"Header1\",\"Header2\"]},"+
         "{\"title\": \"Teachers\",\"selectedImgPath\": \"/images/dashboard_icon_light.png\", \"unselectedImgPath\": \"/images/dashboard_icon_dark.png\", \"controller\":\"com.sms.controllers.DashboardController\",\"view\":\"/sections/Dashboard.fxml\"},"+
         "{\"title\": \"Subjects\",\"selectedImgPath\": \"/images/dashboard_icon_light.png\", \"unselectedImgPath\": \"/images/dashboard_icon_dark.png\", \"controller\":\"com.sms.controllers.DashboardController\",\"view\":\"/sections/Dashboard.fxml\"},"+
         "{\"title\": \"Classes\",\"selectedImgPath\": \"/images/dashboard_icon_light.png\", \"unselectedImgPath\": \"/images/dashboard_icon_dark.png\", \"controller\":\"com.sms.controllers.DashboardController\",\"view\":\"/sections/Dashboard.fxml\"},"+

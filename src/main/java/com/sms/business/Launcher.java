45e65ea13f8f44c064e51e76bbd0c93a930e9759
4A-75-6E-69-6F-72 <ngouoghejunior@gmail.com>
2023-05-29 16:20:05 +0100

    before commenting and cleaning the code

--- src/main/java/com/sms/business/Launcher.java
+++ src/main/java/com/sms/business/Launcher.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 
 public class Launcher {
 
+    // Reflects the call of the app launch from App.java so as to avoid a bug related to using maven
+    // and java
     public static void main(String[] args) {
         Application.launch(App.class, args);
     }
